3b7c23424c5a37437dee83b581d3a55812399232
AlexandruFercal <afercal@cisco.com>
2023-10-28 00:37:54 +0300

    feat(side-panel): style improvements

--- src/components/common/SidePanel/index.tsx
+++ src/components/common/SidePanel/index.tsx
@@ -1,50 +1,81 @@
 import React from 'react'
-import './styles.css'
+
+//material ui
 import SpeedRoundedIcon from '@mui/icons-material/SpeedRounded'
 import GrassRoundedIcon from '@mui/icons-material/GrassRounded'
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded'
 import BarChartRoundedIcon from '@mui/icons-material/BarChartRounded'
+import { Button, Divider, IconButton } from '@mui/material'
+
+//logos
 import mail from '../../assets/mail.png'
 import github from '../../assets/github.png'
 import linkedin from '../../assets/linkedin.png'
-import { IconButton } from '@mui/material'
+
+import './styles.css'
 
 export function SidePanel() {
     return (
         <div className="sidePanel">
-            <div>
-                <h3>Pages</h3>
-                <div className="line"></div>
+            <div className="buttonsContainer">
+                <h2>Pages</h2>
+                <Divider />
                 <div className="sideBtnContainer">
-                    <button className="sideBtn">
-                        <SpeedRoundedIcon /> Greenhouse Dashboard
-                    </button>
-                    <button className="sideBtn">
-                        <GrassRoundedIcon />
-                        My Plants
-                    </button>
-                    <button className="sideBtn">
-                        <SearchRoundedIcon />
-                        Plant Inspector
-                    </button>
-                    <button className="sideBtn">
-                        <BarChartRoundedIcon />
+                    <Button
+                        className="Button"
+                        variant="outlined"
+                        startIcon={<SpeedRoundedIcon sx={{ color: 'black' }} />}
+                    >
+                        Dashboard
+                    </Button>
+                    <Button
+                        className="Button"
+                        variant="outlined"
+                        startIcon={<GrassRoundedIcon sx={{ color: 'black' }} />}
+                    >
+                        My plants
+                    </Button>
+                    <Button
+                        className="Button"
+                        variant="outlined"
+                        startIcon={
+                            <SearchRoundedIcon sx={{ color: 'black' }} />
+                        }
+                    >
+                        Plant inspector
+                    </Button>
+                    <Button
+                        className="Button"
+                        variant="outlined"
+                        startIcon={
+                            <BarChartRoundedIcon sx={{ color: 'black' }} />
+                        }
+                    >
                         Analytics
-                    </button>
+                    </Button>
                 </div>
             </div>
-            <div className="contact">
-                <h3>Contact</h3>
-                <div className="line"></div>
+            <div className="contactContainer">
+                <h2>Contact</h2>
+                <Divider />
                 <div className="contactIcons">
-                    <IconButton>
-                        <img src={github} height="40px"></img>
+                    <IconButton
+                        sx={{ height: '60px', width: '60px', margin: '10px' }}
+                    >
+                        {' '}
+                        <img src={github} height="50px" />
                     </IconButton>
-                    <IconButton>
-                        <img src={mail} height="40px"></img>
+                    <IconButton
+                        sx={{ height: '60px', width: '60px', margin: '10px' }}
+                    >
+                        {' '}
+                        <img src={mail} height="50px" />
                     </IconButton>
-                    <IconButton>
-                        <img src={linkedin} height="40px"></img>
+                    <IconButton
+                        sx={{ height: '60px', width: '60px', margin: '10px' }}
+                    >
+                        {' '}
+                        <img src={linkedin} height="50px" />
                     </IconButton>
                 </div>
             </div>
